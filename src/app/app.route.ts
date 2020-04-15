85d391cfd66ad0c5e34bcd92642d62670ad6336c
Gustavo Hernandez <gustavoh@onetree.com>
2020-04-15 00:48:43 -0300

    Adding routes to app and child routes to usuario component

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,6 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+
 
 
 const APP_ROUTES: Routes =
@@ -11,8 +15,27 @@ const APP_ROUTES: Routes =
         component: HomeComponent
     },
     {
-        path: 'usuario',
-        component: UsuarioComponent
+        path: 'usuario/:id',
+        component: UsuarioComponent,
+        children: [
+            {
+                path: 'nuevo',
+                component: UsuarioNuevoComponent
+            },
+            {
+                path: 'editar',
+                component: UsuarioEditarComponent
+            },
+            {
+                path: 'detalle',
+                component: UsuarioDetalleComponent
+            },
+            {
+                path: '**',
+                pathMatch: 'full',
+                redirectTo: 'nuevo'
+            }
+        ]
     },
     {
         path: '**',
