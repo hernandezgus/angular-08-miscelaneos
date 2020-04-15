78d48d5ba7e83df09b6f3cd7347564544073281f
Gustavo Hernandez <gustavoh@onetree.com>
2020-04-15 00:36:15 -0300

    Adding routes and usuario comoponent

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <p>
+      app-home works!
+    </p>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
